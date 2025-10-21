2ffec8d3755522fbc0d6fddf222331cf157850fe
Jini <wlsdl6685@gmail.com>
2025-10-21 16:37:31 +0900

    feat : 셔틀 안내 내용 수정

--- wedding-invitation-app/src/components/Invitation.tsx
+++ wedding-invitation-app/src/components/Invitation.tsx
@@ -10,16 +10,17 @@ const Invitation = ({ onClose }: { onClose: () => void }) => {
           <h1>초대장</h1>
           <h2>진이 👰🩷🤵 현상</h2>
           <p>진이와 현상이의 결혼식에 초대합니다.</p>
+          <br />
           <p>언제: 2025년 11월 8일 토요일 오후 5시</p>
           <p>어디서: 수원 라온몽드 하우스 웨딩</p>
           <Transportation>
-            <h3>🚎셔틀 안내</h3>
+            <h3>🚎 셔틀 안내</h3>
             <p>
-              ❓308루6671 검정 카니발 <br />
-              📍고용노동부경기지청 앞 버스정류장쪽
+              • 308루6671 검정 카니발 <br />
+              • 고용노동부경기지청 앞 버스정류장
               <br />
-              (성균관대역 1번출구 대각선 율전동성당쪽) <br />
-              🕛시간표 <br />
+              (성균관대역 1번출구 대각선 율전동성당) <br />
+              🕛운행 시간 <br />
               예식 전: 15:40 / 16:00 / 16:20 / 16:45 <br />
               예식 후: 19:00 / 19:30
             </p>
@@ -55,8 +56,8 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 20px 10px;
 
-  padding: 0 10px;
   button {
     position: absolute;
     top: 1rem;
